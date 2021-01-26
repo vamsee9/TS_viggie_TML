6324e084045cc6d14067554bb4e62a95dd88fa73
vamsee9 <4n0n5ur7@protonmail.com>
2021-01-26 22:17:22 +0530

    App: Checkpoint 1
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- veg_ml/lib/Home.dart
+++ veg_ml/lib/Home.dart
@@ -24,11 +24,60 @@ class _HomeState extends State<Home> {
     });
   }
 
-  loadModel() {}
+  void dispsoe() {
+    //Trim memoery
+    super.dispose();
+    Tflite.close();
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 36,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _output = output;
+      _loading = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'assets/model.tfile', labels: 'assets/labels.txt');
+  }
+
+  pickImage() async {
+    var image = await picker.getImage(source: ImageSource.camera);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    classifyImage(_image);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Fruits and Veggies Neural Network',
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w200,
+              fontSize: 20,
+              letterSpacing: 0.8),
+        ),
+      ),
+      body: Container(
+        color: Colors.black.withOpacity(0.9),
+        padding: EdgeInsets.all(30),
+        decoration: BoxDecoration(color: Color(0xFF2A363B)),
+      ),
+    );
   }
 }
