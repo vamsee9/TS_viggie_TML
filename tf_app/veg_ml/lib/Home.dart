b7e369925e18349bffd3f3cf865bfab7719bf68b
vamsee9 <4n0n5ur7@protonmail.com>
2021-01-26 20:11:41 +0530

    App: Basic structure of application
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

---
+++ tf_app/veg_ml/lib/Home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatelessWidget {
+  _HomeState createState() => _HomeState();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _HomeState {
+}
