bf879885710f59860f0cd0d6fb9e53883648fab9
vamsee9 <4n0n5ur7@protonmail.com>
2021-01-26 21:00:57 +0530

    App: initiate base home state
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

---
+++ veg_ml/lib/Home.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _loading = true;
+  File _image;
+  List _output;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value)
+    {
+      setState(() {});
+    });
+}
+
+  loadModel() {}
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
